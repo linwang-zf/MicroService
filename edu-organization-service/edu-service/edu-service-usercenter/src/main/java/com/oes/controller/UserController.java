6268db5f94aebd51ae74da8616b81d862234211e
wanglin <lingwang1313@163.com>
2021-01-09 17:16:20 +0800

    complete

--- edu-organization-service/edu-service/edu-service-usercenter/src/main/java/com/oes/controller/UserController.java
+++ edu-organization-service/edu-service/edu-service-usercenter/src/main/java/com/oes/controller/UserController.java
@@ -18,15 +18,15 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-import static com.sun.activation.registries.LogSupport.log;
-
 
 @RestController
+@Slf4j
 @Api(tags = {"普通用户"})
 public class UserController {
     @Resource
@@ -35,7 +35,7 @@ public class UserController {
     @PostMapping("/user")
     @ApiOperation("用户注册")
     public HttpResult register(@RequestBody UserRegisterQuery query) {
-        log("-----------用户注册-------");
+        log.info("-----------用户注册-------");
         BaseResultDTO resultDTO = userService.register(query);
         if (resultDTO.isSuccess()) {
             return HttpResult.ok(resultDTO.getMessage());
@@ -59,9 +59,9 @@ public class UserController {
     @GetMapping("/user/{id}")
     public HttpResult queryUser(@ApiParam(value = "用户id", example = "1") @PathVariable String id) {
         UserVo user = userService.queryById(Long.valueOf(id));
-        if (user!=null){
-            return HttpResult.ok("查询成功",user);
-        }else return HttpResult.error(HttpStatus.SC_BAD_REQUEST,"查询失败");
+        if (user != null) {
+            return HttpResult.ok("查询成功", user);
+        } else return HttpResult.error(HttpStatus.SC_BAD_REQUEST, "查询失败");
     }
 
     @ApiOperation("根据id删除用户")
@@ -99,31 +99,32 @@ public class UserController {
     @PostMapping("/user/api/{roleName}")
     @ApiOperation("用户注册")
     public HttpResult insert(@RequestBody User user, @PathVariable String roleName) {
-        log("-----------服务间模块调用用户插入-------");
-        return userService.insert(user,roleName);
+        log.info("-----------服务间模块调用用户插入-------");
+        return userService.insert(user, roleName);
     }
+
     //courseContrller
     //teacher
     @GetMapping("/user/auth/api/{userId}")
-    public HttpResult getUserAuthInfo(@PathVariable Integer userId){
+    public HttpResult getUserAuthInfo(@PathVariable Integer userId) {
         return userService.getUserAuthInfoById(userId);
     }
 
     @PostMapping("/user/api/roleName")
-    public boolean addRoles(@RequestBody JSONObject object){
+    public boolean addRoles(@RequestBody JSONObject object) {
         int userId = (int) object.get("userId");
         String roleName = object.getString("roleName");
-        return userService.addRoles(userId,roleName);
+        return userService.addRoles(userId, roleName);
     }
 
     @GetMapping("user/api/roleId")
-    public boolean addRoleById(@PathVariable Integer userId, @PathVariable Integer roleId){
-        return userService.addRole(userId,roleId);
+    public boolean addRoleById(@PathVariable Integer userId, @PathVariable Integer roleId) {
+        return userService.addRole(userId, roleId);
     }
 
     @DeleteMapping("user/api/roleName")
-    public HttpResult removeRole(@PathVariable Integer userId, @PathVariable String roleName){
-        return userService.removeRole(userId,roleName);
+    public HttpResult removeRole(@PathVariable Integer userId, @PathVariable String roleName) {
+        return userService.removeRole(userId, roleName);
     }
 
 
